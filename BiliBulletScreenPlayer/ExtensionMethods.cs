fe183fc3591f57bdc9e3bf3e893fe7f4414ee76d
Poker-sang <poker_sang@outlook.com>
2021-08-10 21:25:07 +0800

    优化项目

---
+++ BiliBulletScreenPlayer/ExtensionMethods.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BiliBulletScreenPlayer
+{
+	public static class ExtensionMethods
+	{
+		public static string ToTime(this double sec) => $"{((int)sec / 60).ToString().PadLeft(2, '0')}:{((int)sec % 60).ToString().PadLeft(2, '0')}";
+
+	}
+}
